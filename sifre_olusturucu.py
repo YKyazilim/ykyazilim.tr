fd77e928d08bda64ae5a5f199fb37d0479fd3079
YKyazilim <159919767+YKyazilim@users.noreply.github.com>
2025-12-10 17:21:35 +0300

    Delete sifre_olusturucu.py

--- sifre_olusturucu.py
+++
@@ -1,8 +0,0 @@
-import random
-karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-sifre_uzunluk = int(input("şifrenizin uzunluğu kaç harflidir?"))
-sifre = ""
-for i in range(sifre_uzunluk):
-    sifre += random.choice(karakterler)
-print(sifre)
-    
